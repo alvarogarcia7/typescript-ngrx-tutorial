a3be79fb59c337b7393a36ec473610adf3ec8c03
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-07-29 21:39:13 +0200

    Create base for the 3N+1 Component

---
+++ src/app/3nplus1/3nplus1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-3nplus1',
+  templateUrl: './3nplus1.component.html',
+  styleUrls: ['./3nplus1.component.css']
+})
+export class _3nplus1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppComponent } from './app.component';
 
 import {StoreModule} from '@ngrx/store';
 import {counterReducer} from './counter.reducer';
-import { MyCounterComponent } from './my-counter/my-counter.component'
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { _3nplus1Component } from './3nplus1/3nplus1.component'
 
 @NgModule({
   imports:      [ BrowserModule,
   StoreModule.forRoot({count: counterReducer}) ],
-  declarations: [ AppComponent, MyCounterComponent ],
+  declarations: [ AppComponent, MyCounterComponent, _3nplus1Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
